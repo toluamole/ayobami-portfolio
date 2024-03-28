304fb55623d43cc8d20dba3de54253997f3ce5f2
toluamole <amoletoluwanimi@gmail.com>
2024-03-28 08:52:19 +0100

    update location in footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -36,7 +36,7 @@ export const Footer = ({display}:IFooter) => {
 					
 			LET&apos;S CONNECT!</Heading>
 			<Text w={['100%','760px']} fontWeight={300} fontSize={['14px','16px']} color={'#fff'} textAlign={'center'} pb={['10px','50px']}>I work at the intersection of brand and product, bringing a heavy sense of graphic design to my digital work. 
-                Currently based in Lagos and open for remote, contract, and freelance opportunities.
+                Currently based in Calgary AB and open for remote, contract, and freelance opportunities.
 			</Text>
 			<Stack direction={ 'row'} justify={'center'} align={['center', 'center']}  spacing={['20px','40px']}>
 				<Link as={RLink} target={'_blank'} to='https://www.behance.net/emmacreative'  color={'#fff'} p={[2,3]} border={'0.25px solid #D3D3D3'} borderRadius={'50%'}><Image w={['16px','20px']} h={['16px','20px']}  src={behance} /></Link>
